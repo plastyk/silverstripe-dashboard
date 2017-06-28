52afd6517d0f6dfa2b9e891667c41b35519deb69
James Turner <git@turnerj.com>
2017-06-28 11:09:24 +0930

    Adding initial test cases

---
+++ tests/DashboardAdminTest.php
@@ -0,0 +1,19 @@
+<?php
+
+class DashboardAdminTest extends FunctionalTest
+{
+    protected static $fixture_file = 'dashboard/tests/DashboardAdminTest.yml';
+
+    public function testPermission()
+    {
+        $dashboardAdmin = DashboardAdmin::create();
+
+        $permittedUser = $this->objFromFixture('Member', 'user1');
+        $this->logInAs($permittedUser);
+        $this->assertTrue($dashboardAdmin->canView());
+
+        $nonPermittedUser = $this->objFromFixture('Member', 'user2');
+        $this->logInAs($nonPermittedUser);
+        $this->assertNotTrue($dashboardAdmin->canView());
+    }
+}
---
+++ tests/panels/UpdatePanelTest.php
@@ -0,0 +1,51 @@
+<?php
+
+class UpdatePanelTest extends FunctionalTest
+{
+    protected static $fixture_file = 'dashboard/tests/panels/UpdatePanelTest.yml';
+
+    public function testPermission()
+    {
+        $updatePanel = UpdatePanel::create();
+
+        $permittedUser = $this->objFromFixture('Member', 'user1');
+        $this->logInAs($permittedUser);
+        $this->assertTrue($updatePanel->canView());
+
+        $nonPermittedUser = $this->objFromFixture('Member', 'user2');
+        $this->logInAs($nonPermittedUser);
+        $this->assertNotTrue($updatePanel->canView());
+    }
+
+    public function testVersionLevel()
+    {
+        $updatePanel = UpdatePanel::create();
+
+        $this->assertEquals('major', $updatePanel->getVersionLevelDifference('3.0.0', '4.0.0'));
+        $this->assertEquals('major', $updatePanel->getVersionLevelDifference('3.1.0', '4.5.0'));
+        $this->assertEquals('major', $updatePanel->getVersionLevelDifference('3.6.1', '4.0.0'));
+        $this->assertEquals('minor', $updatePanel->getVersionLevelDifference('3.0.0', '3.1.0'));
+        $this->assertEquals('minor', $updatePanel->getVersionLevelDifference('3.1.0', '3.5.0'));
+        $this->assertEquals('minor', $updatePanel->getVersionLevelDifference('3.5.1', '3.6.0'));
+        $this->assertEquals('security', $updatePanel->getVersionLevelDifference('3.0.0', '3.0.1'));
+        $this->assertEquals('security', $updatePanel->getVersionLevelDifference('3.1.2', '3.1.5'));
+
+        $this->assertNotTrue($updatePanel->getVersionLevelDifference('3.6.0', '3.6.0'));
+        $this->assertNotTrue($updatePanel->getVersionLevelDifference('4.0.0', '3.6.0'));
+        $this->assertNotTrue($updatePanel->getVersionLevelDifference('3.0', '3.1'));
+    }
+
+    public function testNewestSilverStripeVersion()
+    {
+        $updatePanel = UpdatePanel::create();
+
+        $this->assertTrue($updatePanel->isNewestSilverStripeVersion('3.6.1', '3.6.1'));
+        $this->assertTrue($updatePanel->isNewestSilverStripeVersion('4.0.0', '3.0.0'));
+        $this->assertTrue($updatePanel->isNewestSilverStripeVersion('3.6.0', '3.5.0'));
+        $this->assertTrue($updatePanel->isNewestSilverStripeVersion('3.6.1', '3.6.0'));
+
+        $this->assertNotTrue($updatePanel->isNewestSilverStripeVersion('3.0.0', '4.0.0'));
+        $this->assertNotTrue($updatePanel->isNewestSilverStripeVersion('3.1.0', '3.2.0'));
+        $this->assertNotTrue($updatePanel->isNewestSilverStripeVersion('3.6.0', '3.6.1'));
+    }
+}
