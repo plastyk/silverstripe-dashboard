3f6b9c93d5aff19e09d47f672a014d5829ff1a31
Michael <michael@3dgoo.com>
2017-06-30 11:27:17 +0930

    Search code refactor

--- code/search/DashboardSearchResultFilePanel.php
+++ code/search/DashboardSearchResultFilePanel.php
@@ -2,18 +2,13 @@
 
 class DashboardSearchResultFilePanel extends DashboardSearchResultPanel
 {
-    public function __construct($controller)
-    {
-        parent::__construct($controller, 'File');
-    }
+    protected $className = 'File';
+    protected $searchFields = array('Title', 'Name', 'Content', 'Filename');
+    protected $sort = array('Title' => 'ASC');
+    protected $exclusions = array('ClassName' => 'Folder');
 
     public function canView($member = null)
     {
         return parent::canView($member) && Permission::check('CMS_ACCESS_AssetAdmin') && class_exists('AssetAdmin');
-    }
-
-    public function performSearch($searchValue, $paginationStart = 0, $searchFields = array('Title', 'Name', 'Content', 'Filename'), $sort = array('Title' => 'ASC'), $exclusions = array('ClassName' => 'Folder'))
-    {
-        return parent::performSearch($searchValue, $paginationStart, $searchFields, $sort, $exclusions);
     }
 }
