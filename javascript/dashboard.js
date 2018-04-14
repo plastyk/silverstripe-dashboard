f2d761ddc5a5abe23e9e06380c346a14e9b8720f
michael3dgoo <michael@3dgoo.com>
2018-04-14 19:50:16 +0930

    Fixing link routing JavaScript.

--- javascript/dashboard.js
+++ javascript/dashboard.js
@@ -3,22 +3,23 @@
 
 		$('.cms-content.DashboardAdmin .dashboard-panel a').entwine({
 			onclick: function (e) {
-				var isExternal = $.path.isExternal(this.attr('href'));
+				var baseHref = $('base').attr('href');
+				var baseAdminHref = baseHref + 'admin/';
+				var url = this.attr('href');
+				var isExternal = $.path.isExternal(url) && url.indexOf(baseAdminHref) === -1;
 				if (e.which > 1 || isExternal || this.attr('target') === '_blank' || !this.attr('href')) {
 					return;
 				}
 
 				e.preventDefault();
 
-				var url = this.attr('href');
-
-				if (!isExternal) {
+				if (url.indexOf(baseAdminHref) === -1) {
 					url = $('base').attr('href') + url;
 				}
 
 				if (!$('.cms-container').loadPanel(url)) {
 					if (url.indexOf('admin/pages/') !== -1) {
-						$('.cms-menu-list li#Menu-CMSPagesController').select();
+						$('.cms-menu__list li#Menu-SilverStripe-CMS-Controllers-CMSPagesController').select();
 					}
 					return false;
 				}
