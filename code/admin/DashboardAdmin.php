71b28d43cedad79c8bdfd7f0b1b21b4ba786c9a1
Michael Nowina-Krowicki <michael@3dgoo.com>
2021-04-07 08:12:35 +0930

    SS3 Font Awesome update

--- code/admin/DashboardAdmin.php
+++ code/admin/DashboardAdmin.php
@@ -9,7 +9,7 @@ class DashboardAdmin extends LeftAndMain implements PermissionProvider
     public function init()
     {
         parent::init();
-        Requirements::css('https://use.fontawesome.com/releases/v5.15.1/css/all.css');
+        Requirements::css('https://use.fontawesome.com/releases/v5.15.3/css/all.css');
         Requirements::css(DASHBOARD_ADMIN_DIR . '/css/grid.css');
         Requirements::css(DASHBOARD_ADMIN_DIR . '/css/dashboard.css');
         Requirements::javascript(DASHBOARD_ADMIN_DIR . '/javascript/dashboard.js');
