2fabeb63c109c911b4586925d39a7e9a455f6da5
3Dgoo <michael@3dgoo.com>
2023-03-20 21:54:28 +1030

    Update fontawesome to 6.3.0

--- src/Admin/DashboardAdmin.php
+++ src/Admin/DashboardAdmin.php
@@ -18,7 +18,7 @@ class DashboardAdmin extends LeftAndMain implements PermissionProvider
     public function init()
     {
         parent::init();
-        Requirements::css('https://use.fontawesome.com/releases/v6.1.1/css/all.css');
+        Requirements::css('https://use.fontawesome.com/releases/v6.3.0/css/all.css');
         Requirements::css('plastyk/dashboard:css/dashboard.css');
         Requirements::javascript('plastyk/dashboard:javascript/dashboard.js');
 
